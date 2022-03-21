a927d51b3e0ca7993cab0195001d3dc2f679325a
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-03-21 22:42:15 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -10,9 +10,18 @@ import sqlite3
 import asyncio
 
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+   # from sample_config import Config
+# else:
+   # from config import Config
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+APP_ID = int(os.environ.get("APP_ID", 12345))
+API_HASH = os.environ.get("API_HASH")
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+DOWNLOAD_LOCATION = "./DOWNLOADS"
+TG_MAX_FILE_SIZE = 2097152000
+CHUNK_SIZE = 128
+DB_URI = os.environ.get("DATABASE_URL", "")
+BANNED_USERS = []
 
 from script import script
 
