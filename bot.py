50b74ed63b8d8afd0fe783d1f58766fd9412d152
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-03-21 22:29:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,23 +9,23 @@ if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 #else:
    # from config import Config
-
+from Config import DOWNLOAD_LOCATION, TG_BOT_TOKEN, APP_ID, API_HASH, AUTH_USERS
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 if __name__ == "__main__" :
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
+    if not os.path.isdir(DOWNLOAD_LOCATION):
+        os.makedirs(DOWNLOAD_LOCATION)
     plugins = dict(
         root="plugins"
     )
     app = pyrogram.Client(
         "Rename Bot",
-        bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
+        bot_token=TG_BOT_TOKEN,
+        api_id=APP_ID,
+        api_hash=API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(680815375)
+    AUTH_USERS.add(680815375)
     app.run()
